949ce287801c61a99250dc6e7524a7e180b40454
CrazyBBB <myamamtist@gmail.com>
2017-06-20 11:24:07 +0900

    ダウンロード時にgunzipもするよう変更

--- download.js
+++ download.js
@@ -1,14 +1,17 @@
 const http = require('http');
 const fs = require('fs');
+const zlib = require('zlib');
 
 exports.download = function(url, path) {
+    const gunzip = zlib.createGunzip();
     const outfile = fs.createWriteStream(path);
     
     http.get(url, function(res){
-        res.pipe(outfile);
-        res.on('end', function () {
+        res.pipe(gunzip).pipe(outfile);
+
+        gunzip.on('end', function () {
             outfile.close();
-            console.log("download: " + url);
+            console.log("download: " + path);
         });
     });
 };
--- log.js
+++ log.js
@@ -1,20 +1,20 @@
 const download = require('./download');
 const {DateHourStrIterator} = require('./date-utils');
 
-const startDateHourStr = '2017061322';
-const endDateHourStr   = '2017061403';
+const startDateHourStr = '2017061323';
+const endDateHourStr   = '2017061402';
 
 const d = new DateHourStrIterator(startDateHourStr);
 const urlPrefix   = 'http://tenhou.net/sc/raw/dat/scc';
 const urlPostfix  = '.html.gz';
 const pathPrefix  = './html/scc';
-const pathPostfix = '.html.gz';
+const pathPostfix = '.html';
 
 setTimeout(function repeat() {
     const str = d.nextStr();
 
     download.download(urlPrefix + str + urlPostfix,
-                        pathPrefix + str + pathPostfix)
+                        pathPrefix + str + pathPostfix);
 
     if (str === endDateHourStr) return;
     setTimeout(repeat, 2000);
