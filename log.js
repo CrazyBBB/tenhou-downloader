8227553dd6ae6b1bf693f93b5391c8cab218fde6
CrazyBBB <myamamtist@gmail.com>
2017-06-20 21:58:23 +0900

    今年の分をダウンロードするために書き換え

--- log.js
+++ log.js
@@ -1,11 +1,11 @@
 const download = require('./download');
-const {DateHourStrIterator} = require('./date-utils');
+const {DateStrIterator} = require('./date-utils');
 
-const startDateHourStr = '2017061323';
-const endDateHourStr   = '2017061402';
+const startDateStr = '20170101';
+const endDateStr   = '20170611';
 
-const d = new DateHourStrIterator(startDateHourStr);
-const urlPrefix   = 'http://tenhou.net/sc/raw/dat/scc';
+const d = new DateStrIterator(startDateStr);
+const urlPrefix   = 'http://tenhou.net/sc/raw/dat/2017/scc';
 const urlPostfix  = '.html.gz';
 const pathPrefix  = './html/scc';
 const pathPostfix = '.html';
@@ -16,6 +16,6 @@ setTimeout(function repeat() {
     download.download(urlPrefix + str + urlPostfix,
                         pathPrefix + str + pathPostfix);
 
-    if (str === endDateHourStr) return;
+    if (str === endDateStr) return;
     setTimeout(repeat, 2000);
 }, 2000);
